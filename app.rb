cced8168c8d9d56c4de3ca0190e2288c416cb185
5-4 <github email address>
2017-12-19 00:05:16 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	configure do
+		enable :sessions
+		set :session_secret, "5-4"
+	end
+
+	get '/' do
+		erb :index
+	end
+
+	post '/checkout' do
+		session["item"] = params[:item]
+		@session = session
+		erb :cart
+	end
+
 end
